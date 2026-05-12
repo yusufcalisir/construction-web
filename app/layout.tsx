25e9b7a88fe3cda23682724aee30322bf35c6b8e
yusufcalisir <ysfcals@gmail.com>
2026-05-12 14:34:15 +0300

    feat: add Google Tag Manager and update Content Security Policy for analytics and ads tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import { LanguageProvider } from '@/components/LanguageProvider'
 import { ThemeProvider } from '@/components/ThemeProvider'
@@ -416,6 +417,19 @@ export default function RootLayout({
         <meta name="rating" content="general" />
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MMZXVCDS');
+          `}
+        </Script>
         <ThemeProvider>
           <LanguageProvider>
             {children}
